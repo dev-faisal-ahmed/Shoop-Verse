ae5170897364d6458e1dc479d0efa1b1dfa830df
Faisal Ahmed <ost.faisal.ahmed@gmail.com>
2025-09-20 01:09:34 +0600

    feat : implemented auth guard

---
+++ src/auth/guard/auth.guard.ts
@@ -0,0 +1,49 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+import { UnitOfWork } from 'src/unit-of-work';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  private bearer = 'bearer';
+
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly uow: UnitOfWork,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest<Request>();
+    const token = this.extractToken(request);
+    if (!token) throw new UnauthorizedException('No token has been provided');
+
+    try {
+      const payload = this.jwtService.verify<TokenPayload>(token);
+      if (!payload) throw new UnauthorizedException('Invalid token');
+
+      const user = await this.uow.user.findByEmail(payload.email);
+      if (!user) throw new UnauthorizedException('User not found');
+      request['user'] = user;
+      return true;
+    } catch (error) {
+      throw new UnauthorizedException(
+        error instanceof Error ? error.message : 'Invalid token',
+      );
+    }
+  }
+
+  private extractToken(request: Request): string {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    if (type.toLowerCase() !== this.bearer) return '';
+    return token;
+  }
+}
+
+// type
+type TokenPayload = { id: string; name: string; email: string };
